d67dd4915fc9ed5be2ea79e5e2c45fe4a6a5a5cd
83mbyte <registered.mail.mb@gmail.com>
2023-08-31 11:44:07 +0300

    init commit

---
+++ src/lib/promptsAPI.js
@@ -0,0 +1,36 @@
+import { promptsTemplates } from "./promptsDefaults";
+
+export const promptsAPI = {
+    createSystemMessage: (activeButton) => {
+        console.log('run promptsAPI.create()');
+        let length;
+        switch (activeButton.length) {
+            case 'Short':
+                length = '50 words';
+                break;
+
+            case 'Medium':
+                length = '100 words';
+                break;
+            case 'Long':
+                length = '200 words';
+                break;
+            case 'eXtraLong':
+                length = '500 words';
+                break;
+            default:
+                length = '30 words';
+                break;
+        }
+
+        // provide your prompt as the example below
+        // 
+        // return ({
+        //     role: 'system',
+        //     content: `You are helpful assistant. Reply in a funny manner.`
+        // })
+
+        return promptsTemplates.main(activeButton)
+    }
+
+}
