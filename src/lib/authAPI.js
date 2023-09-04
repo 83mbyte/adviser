fb7b6daded7d6caa49cb4b669f131ad826d74fb2
83mbyte <registered.mail.mb@gmail.com>
2023-09-04 15:00:14 +0300

    signOut method added

--- src/lib/authAPI.js
+++ src/lib/authAPI.js
@@ -1,13 +1,14 @@
-import { app } from '../utilities/_f_i_r_e_base';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { app } from '../_f_i_r_e_base/_f_i_r_e_base';
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 
 
 export const authAPI = {
-    signIn: () => {
+    signIn: async (email, password) => {
         const auth = getAuth(app);
-        signInWithEmailAndPassword(auth, email, password)
+        return await signInWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
+                console.log('...still logging')
                 const user = userCredential.user;
 
                 return user
@@ -16,6 +17,17 @@ export const authAPI = {
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
+                return ({ errorCode, errorMessage })
             });
+    },
+
+    signOut: () => {
+        const auth = getAuth(app);
+        signOut(auth).then(() => {
+            console.log('SignOut done')
+            // Sign-out successful.
+        }).catch((error) => {
+            // An error happened.
+        });
     }
 }
