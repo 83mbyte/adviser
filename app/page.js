1d63300ca52a3fd65d0b1b63d036b222bfbbe56c
83mbyte <registered.mail.mb@gmail.com>
2023-09-04 14:36:18 +0300

    Login component added

--- app/page.js
+++ app/page.js
@@ -1,19 +1,12 @@
 
-import { getData } from '@/src/lib/fetchData';
-import AppClient from '../src/components/AppClient';
-
 import styles from './page.module.css';
+import Login from '@/src/components/Login/Login';
 
 export default async function Home() {
 
-  const empty = {};
-  const chatHistoryObj = await getData();;
-
   return (
     <div className={styles.mainOuterContainer}>
-      {
-        chatHistoryObj.status === 'success' ? <AppClient chatHistoryObj={chatHistoryObj.data} /> : <AppClient chatHistoryObj={empty} />
-      }
+      <Login />
     </div>
   )
 }
