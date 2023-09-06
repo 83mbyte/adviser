07dce7f7612f97f2124bbc0186e493a72f385a4e
83mbyte <registered.mail.mb@gmail.com>
2023-09-06 16:24:10 +0300

    config values edited

--- src/_f_i_r_e_base/_f_i_r_e_base.js
+++ src/_f_i_r_e_base/_f_i_r_e_base.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    projectId: "adviserai",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBAS_STORAGE_BUCKET,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: "562034705604",
-    appId: "1:562034705604:web:54f1ef9961bb22b96ab591"
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
