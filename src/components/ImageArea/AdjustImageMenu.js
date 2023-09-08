207d6b16a42db96abc60fdd60b344fa45ce8e18f
83mbyte <registered.mail.mb@gmail.com>
2023-09-08 18:01:59 +0300

    deleted useless spaces in the code

--- src/components/ImageArea/AdjustImageMenu.js
+++ src/components/ImageArea/AdjustImageMenu.js
@@ -17,15 +17,11 @@ const AdjustImageMenu = ({ themeColor, size, setSize }) => {
                 aria-label='Options'
                 icon={<TbAdjustmentsHorizontal size={'22px'} />}
                 variant='outline'
-
                 colorScheme={themeColor}
-
             />
             <MenuList  >
                 <MenuOptionGroup defaultValue={size} title='Image Size' type='radio' onChange={setSize} >
-                    <MenuItemOption value='256' fontSize={'sm'} _hover={{ backgroundColor: `${themeColor}.50` }}
-
-                    >256x256 pixels</MenuItemOption>
+                    <MenuItemOption value='256' fontSize={'sm'} _hover={{ backgroundColor: `${themeColor}.50` }}>256x256 pixels</MenuItemOption>
                     <MenuItemOption value='512' fontSize={'sm'} _hover={{ backgroundColor: `${themeColor}.50` }}>512x512 pixels</MenuItemOption>
                     <MenuItemOption value='1024' fontSize={'sm'} _hover={{ backgroundColor: `${themeColor}.50` }}>1024x1024 pixels</MenuItemOption>
                 </MenuOptionGroup>
