6aa7efe982a1144c504f1c02e1b6b4dd616bb079
83mbyte <registered.mail.mb@gmail.com>
2023-09-08 17:46:07 +0300

    added 'role' parameter in getReplyFromAssistant

--- src/lib/fetchData.js
+++ src/lib/fetchData.js
@@ -50,12 +50,28 @@ export const getData = async () => {
     return await makeRequest(URL, options);
 }
 
-export const getReplyFromAssistant = async (data) => {
+export const getReplyFromAssistant = async (data, model) => {
     let options = {
         method: 'POST',
         body: JSON.stringify(data)
     }
-    let resp = await fetch(process.env.NEXT_PUBLIC_FUNC_PROD_URL, options);
+    let resp;
+    switch (model) {
+
+        case 'chat':
+            resp = await fetch(process.env.NEXT_PUBLIC_FUNC_DEV_URL, options);
+            // resp = await fetch(process.env.NEXT_PUBLIC_FUNC_PROD_URL, options);
+            break;
+        case 'image':
+            resp = await fetch(process.env.NEXT_PUBLIC_FUNC_IMAGE_DEV_URL, options);
+            // resp = await fetch(process.env.NEXT_PUBLIC_FUNC_IMAGE_PROD_URL, options);
+            break;
+
+        default:
+            resp = await fetch(process.env.NEXT_PUBLIC_FUNC_DEV_URL, options);
+            // resp = await fetch(process.env.NEXT_PUBLIC_FUNC_PROD_URL, options);
+            break;
+    }
 
     if (resp) {
         return await resp.json();
