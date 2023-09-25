89b6a51006e94bb2e4ea5aa5dc05299cb7eab8d2
83mbyte <registered.mail.mb@gmail.com>
2023-09-25 17:24:22 +0300

    renders IndexPage component

--- app/page.js
+++ app/page.js
@@ -1,14 +1,34 @@
 
 import { NecessaryProviders } from '@/src/context/providers';
 import styles from './page.module.css';
-import Login from '@/src/components/Login/Login';
+import IndexPage from '@/src/components/IndexPage/IndexPage';
+import { dbAPI } from '@/src/lib/dbAPI';
+
+
+const getData = async (sectionName) => {
+  const resp = await dbAPI.getSectionData(sectionName);
+  if (!resp) {
+    throw new Error('Failed to fetch data')
+  }
+  return resp
+}
+
+
 
 export default async function Home() {
+  let features;
+  let pricing;
+  try {
+    features = await getData('features');
+    pricing = await getData('pricing')
+  } catch (error) {
+    console.error(error)
+  }
 
   return (
     <div className={styles.mainOuterContainer}>
       <NecessaryProviders>
-        <Login />
+        <IndexPage data={{ features, pricing }} />
       </NecessaryProviders>
 
 
