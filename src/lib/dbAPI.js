e26d4042029565662e60cb9386eed5b188b59edf
83mbyte <registered.mail.mb@gmail.com>
2023-09-25 17:19:45 +0300

    getSectionData added

--- src/lib/dbAPI.js
+++ src/lib/dbAPI.js
@@ -3,7 +3,20 @@ import { app } from '../_f_i_r_e_base/_f_i_r_e_base';
 
 import { doc, getDoc } from "firebase/firestore";
 export const dbAPI = {
+    getSectionData: async (sectionName) => {
+        const db = getFirestore(app);
+        const docRef = doc(db, 'serviceData', sectionName);
+        const docSnap = await getDoc(docRef);
 
+        if (docSnap.exists()) {
+            // console.log("Document data:", docSnap.data());
+            return docSnap.data()
+        } else {
+            // docSnap.data() will be undefined in this case
+            console.log("No such document!");
+            return null
+        }
+    },
 
     getData: async (userId) => {
         const db = getFirestore(app);
