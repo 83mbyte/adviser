65041b1fbf06e88821210095a9b983f3e725b452
83mbyte <registered.mail.mb@gmail.com>
2023-09-26 18:11:25 +0300

    authAPI methods now return object as a result.

--- src/lib/authAPI.js
+++ src/lib/authAPI.js
@@ -1,5 +1,5 @@
 import { app } from '../_f_i_r_e_base/_f_i_r_e_base';
-import { getAuth, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithRedirect, getRedirectResult, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithRedirect, getRedirectResult, createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from 'firebase/auth'
 
 
 export const authAPI = {
@@ -11,13 +11,13 @@ export const authAPI = {
                 console.log('...still logging')
                 const user = userCredential.user;
 
-                return user
+                return { status: 'ok', user }
                 // ...
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                return ({ errorCode, errorMessage })
+                return ({ status: 'error', errorCode, errorMessage })
             });
     },
 
@@ -40,7 +40,7 @@ export const authAPI = {
                 const user = result.user;
                 // IdP data available using getAdditionalUserInfo(result)
                 // ...
-                return user
+                return { status: 'ok', user }
             }).catch((error) => {
                 // Handle Errors here.
                 const errorCode = error.code;
@@ -50,22 +50,31 @@ export const authAPI = {
                 // The AuthCredential type that was used.
                 const credential = GoogleAuthProvider.credentialFromError(error);
                 // ...
-                return ({ errorCode, errorMessage })
+                return ({ status: 'error', errorCode, errorMessage })
             });
     },
 
-    signUp: async (email, password) => {
+    signUp: async (email, password, firstName = '', lastName = '') => {
         const auth = getAuth(app);
         return await createUserWithEmailAndPassword(auth, email, password)
             .then((userCredential) => {
                 // Signed in 
                 const user = userCredential.user;
-                return user
+                if (lastName !== '' || firstName != '') {
+                    let name = [firstName, lastName];
+                    updateProfile(auth.currentUser, { displayName: name.join(' ') })
+                        .then(() => null)
+                }
+                return sendEmailVerification(auth.currentUser)
+                    .then(() => {
+                        return { status: 'ok', message: 'verify-email', user: auth.currentUser }
+                    })
+                // return user
             })
             .catch((error) => {
                 const errorCode = error.code;
                 const errorMessage = error.message;
-                return ({ errorCode, errorMessage })
+                return ({ status: 'error', errorCode, errorMessage })
             });
     },
 
