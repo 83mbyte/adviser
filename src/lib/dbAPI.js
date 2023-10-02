6df9051482a63715ca96633dcce048b6b67d45e6
83mbyte <registered.mail.mb@gmail.com>
2023-10-02 11:49:33 +0300

    getUserData added

--- src/lib/dbAPI.js
+++ src/lib/dbAPI.js
@@ -54,4 +54,20 @@ export const dbAPI = {
         return 'chat removed'
     },
 
+    // users
+    getUserData: async (userId) => {
+        const db = getFirestore(app);
+        const docRef = doc(db, 'users', userId);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            //console.log("Document data:", docSnap.data());
+            return docSnap.data()
+        } else {
+            // docSnap.data() will be undefined in this case
+            console.log("No such document!");
+            return null
+        }
+    },
+
 }
