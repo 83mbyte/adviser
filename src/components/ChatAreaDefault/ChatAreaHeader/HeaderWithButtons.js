4c50829ab193764a29d2aa792078a5353dc412d2
83mbyte <registered.mail.mb@gmail.com>
2023-10-04 18:55:15 +0300

    added IconButton with ChatSettingsAnimatedIcon

--- src/components/ChatAreaDefault/ChatAreaHeader/HeaderWithButtons.js
+++ src/components/ChatAreaDefault/ChatAreaHeader/HeaderWithButtons.js
@@ -1,8 +1,11 @@
-import { Box, HStack, IconButton, Text } from '@chakra-ui/react';
+import { Box, HStack, IconButton, Spacer, Text } from '@chakra-ui/react';
 
-import { MdOutlineNotes, MdApps } from 'react-icons/md';
+import { MdOutlineNotes, MdApps, } from 'react-icons/md';
 
-const HeaderWithButtons = ({ toggleShowTopics, themeColor, togglePredefinedList, showIconList }) => {
+import ChatSettingsAnimatedIcon from './ChatSettingsAnimatedIcon';
+
+
+const HeaderWithButtons = ({ toggleShowTopics, themeColor, togglePredefinedList, showIconList, setShowChatSettings, showChatSettings, setShowPredefined }) => {
     return (
 
         <Box bg={'#FAFAFA'}
@@ -14,24 +17,37 @@ const HeaderWithButtons = ({ toggleShowTopics, themeColor, togglePredefinedList,
             borderBottomWidth={'1px'}
             borderBottomColor={'gray.200'}
         >
-            <HStack alignItems={'center'} justifyContent={'flex-start'} bg='' w='full'>
-
-                <IconButton icon={<MdApps size={'22px'} />} colorScheme={themeColor} variant={'ghost'} onClick={toggleShowTopics} size={'sm'}
-                    p={2}
-                />
-                {/* <Box>
-                    <Text bg='' w='full' fontSize={'xs'} textAlign={'center'} mb={0} lineHeight={'3'}>Assistance with</Text>
-                    <Text bg='' w='full' fontSize={'md'} textAlign={'center'} fontWeight={'600'} lineHeight={'4'}>{selectedTopic}</Text>  
-                </Box> */}
-
-                {showIconList &&
-                    <IconButton icon={<MdOutlineNotes size={'28px'} />} colorScheme={themeColor} variant={'ghost'} onClick={togglePredefinedList} size={'sm'}
+            <HStack justifyContent={'space-between'}>
+                <HStack bg=''>
+                    <IconButton icon={<MdApps size={'22px'} />} colorScheme={themeColor} variant={'ghost'} onClick={toggleShowTopics} size={'sm'}
                         p={2}
-                    />}
+                    />
 
+                    {showIconList &&
+                        <IconButton icon={<MdOutlineNotes size={'28px'} />} colorScheme={themeColor} variant={'ghost'}
+                            onClick={() => { setShowChatSettings(false); togglePredefinedList() }} size={'sm'}
+                            p={2}
+                        />
+                    }
+
+                </HStack>
+                <HStack bg='' w='100%'>
+                    {
+                        showChatSettings == true ? <Text textAlign={'center'} w="100%" bg='' marginLeft={'-40px'}>Chat Settings</Text> : <Spacer w={'100%'} />
+                    }
+
+                    <IconButton
+                        icon={<ChatSettingsAnimatedIcon color={themeColor} size={'26px'} />}
+                        variant={'ghost'}
+                        colorScheme={themeColor}
+                        size={'sm'}
+                        onClick={() => { setShowPredefined(false); setShowChatSettings(!showChatSettings); }}
+                    />
+                </HStack>
             </HStack>
         </Box>
     );
 };
 
 export default HeaderWithButtons;
+
