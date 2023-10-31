aa0fe525a9fbafd0194dea91585bcc88ca4b05e0
83mbyte <registered.mail.mb@gmail.com>
2023-10-31 15:10:56 +0200

    SignUp component wrapped with Suspence

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,9 +1,14 @@
 import SignUp from "@/src/site_pages/SignUpPage/SignUp";
+import { Suspense } from "react";
 
-
+const SignUpFallback = () => {
+    return (<div style={{ display: 'flex', flexDirection: 'column', flex: 1, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}> <p style={{ textAlign: 'center' }}>loading..</p></div>)
+}
 export default async function Signup_Page() {
 
     return (
-        <SignUp />
+        <Suspense fallback={<SignUpFallback />}>
+            <SignUp />
+        </Suspense>
     )
 }
