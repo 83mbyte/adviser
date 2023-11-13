7b692aad489821272f01bea3eaad3d714623f9c0
83mbyte <registered.mail.mb@gmail.com>
2023-11-13 17:58:44 +0200

    MotionModalSignOut and MotionZoomImgModal removed

--- src/site_pages/WorkspacePage/Workspace.js
+++ src/site_pages/WorkspacePage/Workspace.js
@@ -1,12 +1,19 @@
+
+import { useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 import { Box, Portal } from '@chakra-ui/react';
 
 import styles from './WorkspaceStyles.module.css';
 import Header from '../../components/Workspace/Header/Header';
 import MainArea from '../../components/Workspace/MainArea/MainArea';
 import Footer from '../../components/PagesFooter/Footer';
-import MotionModalSignOut from '@/src/components/Modal/MotionSignOutModal';
-import MotionZoomImgModal from '@/src/components/Modal/MotionZoomImgModal';
 import { useSettingsContext } from '@/src/context/SettingsContext';
+import { AnimatePresence, motion } from 'framer-motion';
+import { animationProps } from '@/src/lib/animationProps';
+import SignOutModal from '@/src/components/Modal/SignOutModal';
+import ZoomImgModal from '@/src/components/Modal/ZoomImgModal';
+
+import CheckoutResultModal from '@/src/components/Modal/CheckoutResultModal';
 
 const Workspace = () => {
 
@@ -14,10 +21,23 @@ const Workspace = () => {
     const showModalSettings = userUISettings.showModalWindow;
     const userWorkspaceType = userUISettings.userWorkspaceType;
 
+    const params = useSearchParams();
+
     const closeModal = () => {
         showModalSettings.setShowModal({ isShow: false, type: null, body: null });
+        history.pushState(null, "", "workspace");
         return null
     }
+
+    const gotoCheckout = () => {
+        userWorkspaceType.setWorkspaceType('subscription');
+    }
+
+    useEffect(() => {
+        if (params.has('checkout')) {
+            showModalSettings.setShowModal({ isShow: true, type: 'CheckoutResult', body: params.get('checkout') });
+        }
+    }, [])
 
     return (
         <>
@@ -47,16 +67,45 @@ const Workspace = () => {
             <Box as='footer' className={styles.footer}>
                 <Footer type={'workspace'} />
             </Box>
-            <Portal>
-                <MotionModalSignOut
-                    showModal={showModalSettings.showModal}
-                    handleClose={closeModal}
-                />
 
-                <MotionZoomImgModal
-                    showModal={showModalSettings.showModal}
-                    handleClose={closeModal}
-                />
+            <Portal>
+                <AnimatePresence mode='wait'>
+
+                    {
+                        showModalSettings.showModal.isShow == true &&
+                        <motion.div
+                            onClick={closeModal}
+                            key={'backdrop'}
+                            style={{ zIndex: 3001, position: 'absolute', top: 0, left: 0, height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.7)' }}
+                            variants={animationProps.opacity}
+                            initial={'hidden'}
+                            animate={'show'}
+                            exit={'exit'}
+                        >
+                            <motion.div
+                                key={'modalInner'}
+                                variants={animationProps.slideFromTop}
+                                initial={'hidden'}
+                                animate={'visible'}
+                                exit={'exit'}
+                            >
+
+                                {
+                                    showModalSettings.showModal.type === 'SignOut' &&
+                                    <SignOutModal handleClose={closeModal} />
+                                }
+                                {showModalSettings.showModal.type === 'ZoomImgModal' &&
+                                    <ZoomImgModal handleClose={closeModal} image={showModalSettings.showModal.body && showModalSettings.showModal.body !== undefined ? showModalSettings.showModal.body : null} />
+                                }
+                                {
+                                    showModalSettings.showModal.type === 'CheckoutResult' &&
+                                    <CheckoutResultModal handleClose={closeModal} result={showModalSettings.showModal.body} renewCheckout={gotoCheckout} />
+                                }
+                            </motion.div>
+                        </motion.div>
+                    }
+                </AnimatePresence>
+
             </Portal>
         </>
     )
