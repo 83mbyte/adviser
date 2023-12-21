004bd946e03d4174f55cc9943853d95a784a6ab7
83mbyte <registered.mail.mb@gmail.com>
2023-12-21 11:12:18 +0200

    firebase/analytics added

--- src/_f_i_r_e_base/_f_i_r_e_base.js
+++ src/_f_i_r_e_base/_f_i_r_e_base.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,10 +14,15 @@ const firebaseConfig = {
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: "562034705604",
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Initialize Analytics
+const analytics = getAnalytics(app);
+
 // Initialize Firestore
 export const db = initializeFirestore(app, { experimentalAutoDetectLongPolling: false, experimentalForceLongPolling: true });
