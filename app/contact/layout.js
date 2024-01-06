ccd7bc0d10d3c9d9f3ada9faedf2a0c452d096f6
83mbyte <registered.mail.mb@gmail.com>
2024-01-06 13:24:37 +0200

    page meta description updated

--- app/contact/layout.js
+++ app/contact/layout.js
@@ -2,7 +2,7 @@ import { NecessaryProviders } from "@/src/context/providers"
 
 export const metadata = {
     title: 'Helpi - Contact Us',
-    description: 'Helpi - Your personal AI-based assistant. Use our contact form in order to get in touch with our staff.',
+    description: 'Helpi - Your personal AI-based assistant. Use our contact form in order to get in touch with our staff. Contact us for any inquiries or assistance you may need while your work with our Helpi AI bot. Our dedicated customer support team is ready to provide prompt and reliable solutions to your questions.',
     manifest: './icons/site.webmanifest',
 }
 
