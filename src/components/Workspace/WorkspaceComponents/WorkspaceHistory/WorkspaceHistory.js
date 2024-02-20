e83bd0a1060f76d4f3be5b6e3bdb50fc7dcb7768
83mbyte <registered.mail.mb@gmail.com>
2024-02-20 11:38:06 +0200

    ChatAllHistory replaced by WorkspaceHistory component

--- src/components/Workspace/WorkspaceComponents/WorkspaceHistory/WorkspaceHistory.js
+++ src/components/Workspace/WorkspaceComponents/WorkspaceHistory/WorkspaceHistory.js
@@ -1,18 +1,17 @@
-
+import { useState } from 'react';
 import { Box, HStack, Icon, IconButton, StackDivider, VStack } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { MdDelete } from 'react-icons/md';
-import { useState } from 'react';
-
 import { RiChatHistoryLine } from "react-icons/ri";
+
 import { animationProps } from '@/src/lib/animationProps';
 
+const WorkspaceHistory = ({ themeColor, allHistory, clearItemFromHistory, chooseHistory, type }) => {
 
-const ChatAllHistory = ({ themeColor, allHistory, clearChatFromHistory, chooseHistory }) => {
     const [isLoading, setIsLoading] = useState(false);
-    const onClickDeleteHandler = async (chatId) => {
+    const onClickDeleteHandler = async (historyId) => {
         setIsLoading(true);
-        let res = await clearChatFromHistory(chatId);
+        await clearItemFromHistory(historyId);
         setIsLoading(false);
     }
 
@@ -26,7 +25,9 @@ const ChatAllHistory = ({ themeColor, allHistory, clearChatFromHistory, chooseHi
             mt={4}
         >
             {
-                Object.keys(allHistory).map((chatId, index) => {
+                (allHistory && Object.keys(allHistory).length > 0) &&
+                Object.keys(allHistory).map((historyId, index) => {
+
                     return (
                         <HStack w='full' spacing={'1'} color={`${themeColor}.800`} key={`history_${index}`}
                             as={motion.div}
@@ -36,14 +37,19 @@ const ChatAllHistory = ({ themeColor, allHistory, clearChatFromHistory, chooseHi
                             custom={index}
                             layout
                         >
-                            <Box bg='' pr={1} display={'flex'} alignItems={'center'}>
+                            <Box bg='' px={1} display={'flex'} alignItems={'center'}>
                                 <Icon as={RiChatHistoryLine} />
                             </Box>
                             <Box bg='' w='full'
                                 _hover={{ cursor: 'pointer' }}
-                                onClick={() => chooseHistory(`${chatId}`)}
+                                onClick={() => chooseHistory(`${historyId}`)}
                             >
-                                {`${allHistory[chatId][0].user.content.slice(0, 80)}...`}
+                                {type == 'chat'
+                                    && `${allHistory[historyId][0].user.content.slice(0, 80)}...`
+                                }
+                                {type == 'summarize'
+                                    && `${allHistory[historyId][0].assistant.title.slice(0, 80)}...`
+                                }
                             </Box>
                             <Box bg=''
                                 as={motion.div}
@@ -59,17 +65,15 @@ const ChatAllHistory = ({ themeColor, allHistory, clearChatFromHistory, chooseHi
                                     colorScheme={themeColor}
                                     _hover={{ color: 'red' }}
                                     variant={'link'}
-                                    onClick={() => onClickDeleteHandler(chatId)}
+                                    onClick={() => onClickDeleteHandler(historyId)}
                                 />
                             </Box>
                         </HStack>
-
                     )
                 })
             }
         </VStack>
-    )
-}
+    );
+};
 
-
-export default ChatAllHistory;
+export default WorkspaceHistory;
