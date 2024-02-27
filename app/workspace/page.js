cffd9511bff92e909fd93b5b372e550277747bcc
83mbyte <registered.mail.mb@gmail.com>
2024-02-27 19:28:38 +0200

    changed according to new context structure

--- app/workspace/page.js
+++ app/workspace/page.js
@@ -3,55 +3,93 @@
 import { useAuthContext } from "@/src/context/AuthContextProvider";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+import { dbAPI } from "@/src/lib/dbAPI";
+
+import HistoryContextProvider from "@/src/context/HistoryContext/HistoryContextProvider";
+import PredefinedDataContextProvider from "@/src/context/PredefinedDataContext/PredefinedDataContextProvider";
+import SettingsContextProvider from "@/src/context/SettingsContext/SettingsContextProvider";
 
 import Workspace from "@/src/site_pages/WorkspacePage/Workspace";
-import PredefinedDataContextProvider from "@/src/context/PredefinedDataContextProvider";
-import HistoryContextProvider from "@/src/context/HistoryContextProvider";
-import SettingsContextProvider from "@/src/context/SettingsContext";
 
 export default function WorkspacePage() {
     const user = useAuthContext();
     const router = useRouter();
 
+    const [historyFromDB, setHistoryFromDB] = useState(null);
+    const [predefinedDataFromDB, setPredefinedDataFromDB] = useState(null);
+    const [settingsFromDB, setSettingsFromDB] = useState(null);
+
     useEffect(() => {
+        const getUserData = async (userId) => {
+            let resp = await dbAPI.getUserFullData(userId);
+
+            if (resp.status == 'Success') {
+                setHistoryFromDB({ historyChats: resp.payload.chats, historySummarizeYT: resp.payload.summarizeYT });
+                setSettingsFromDB(resp.payload.settings);
+            }
+        }
+
+        const getPredefinedData = async () => {
+            let resp = await dbAPI.getPredefinedData_new('prompts');
+
+            if (resp.status == 'Success') {
+                setPredefinedDataFromDB(resp.payload);
+            }
+        }
+
         if (user === null || user === undefined) {
             router.push('/')
+        } {
+            getPredefinedData();
+            getUserData(user.uid);
         }
     }, [user]);
 
     return (
         <>
+
             {
                 !user
-                    ? <div style={
-                        {
-                            backgroundColor: '',
-                            height: '100vh',
-                            display: 'flex',
-                            flex: 1,
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                            justifyContent: 'center'
-                        }
-                    }
-                    >
-                        <div style={{ border: '1px solid rgb(225,180,180)', fontSize: '26px', padding: '20px', }}>Access denied!</div>
-                    </div>
-                    :
-                    <PredefinedDataContextProvider>
-                        <HistoryContextProvider>
-                            <SettingsContextProvider>
+                    ? <WarningMessage message={'Access denied!'} />
+                    : <>
+                        <PredefinedDataContextProvider data={predefinedDataFromDB}>
+                            <HistoryContextProvider data={historyFromDB}>
 
-                                <Workspace />
+                                <SettingsContextProvider data={settingsFromDB}>
+                                    <Workspace />
+                                </SettingsContextProvider>
 
-                            </SettingsContextProvider>
-                        </HistoryContextProvider>
-                    </PredefinedDataContextProvider>
+                            </HistoryContextProvider>
+                        </PredefinedDataContextProvider>
+                    </>
             }
         </>
+
     )
 
 
 }
+
+
+const WarningMessage = ({ message }) => {
+    return (
+        <div style={
+            {
+                backgroundColor: '',
+                height: '100vh',
+                display: 'flex',
+                flex: 1,
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }
+        }
+        >
+            <div style={{ border: '1px solid rgb(225,180,180)', fontSize: '26px', padding: '20px', }}>{message}</div>
+        </div>
+    )
+}
+
 
