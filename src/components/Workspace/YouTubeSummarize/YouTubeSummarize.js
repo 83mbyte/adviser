cb37b5219e6dcc29a63415ac9a6d69f5b43b4d58
83mbyte <registered.mail.mb@gmail.com>
2025-01-21 12:34:54 +0200

    ytsummarize service was disabled

--- src/components/Workspace/YouTubeSummarize/YouTubeSummarize.js
+++ src/components/Workspace/YouTubeSummarize/YouTubeSummarize.js
@@ -4,24 +4,24 @@ import { getReplyFromAssistant } from "@/src/lib/fetchingData";
 import { useAuthContext } from "@/src/context/AuthContextProvider";
 import { useToast } from "@chakra-ui/react";
 
-import { animationProps } from "@/src/lib/animationProps";
+// import { animationProps } from "@/src/lib/animationProps";
 
 import { dbAPI } from "@/src/lib/dbAPI";
 import { promptTemplatesAPI } from "@/src/lib/promptsAPI";
 
 import { MdHistory, } from 'react-icons/md';
-import { AnimatePresence, motion } from "framer-motion";
+// import { AnimatePresence, motion } from "framer-motion";
 
-import WorkspaceHistory from "../WorkspaceComponents/WorkspaceHistory/WorkspaceHistory";
-import WorkspaceCard from "../WorkspaceComponents/WorkspaceCard";
-import ResultContentYTSummarize from "../WorkspaceComponents/WorkspaceResultsToShow/ResultContentYTSummarize.js"
+// import WorkspaceHistory from "../WorkspaceComponents/WorkspaceHistory/WorkspaceHistory";
+// import WorkspaceCard from "../WorkspaceComponents/WorkspaceCard";
+// import ResultContentYTSummarize from "../WorkspaceComponents/WorkspaceResultsToShow/ResultContentYTSummarize.js"
 import { useSettingsContext } from "@/src/context/SettingsContext/SettingsContextProvider";
 import { useHistoryContext } from "@/src/context/HistoryContext/HistoryContextProvider";
 import UnavailableResource from "../../Modal/UnavailableResource";
 
 
-const headerLeftButtons = null;
-const headerRightButtons = null;
+// const headerLeftButtons = null;
+// const headerRightButtons = null;
 
 const YouTubeSummarize = ({ showNoHistoryVideoIssue, setShowNoHistoryVideoIssue }) => {
 
@@ -433,8 +433,8 @@ const YouTubeSummarize = ({ showNoHistoryVideoIssue, setShowNoHistoryVideoIssue 
     return (
         <>
 
-            {/* <UnavailableResource /> */}
-            <WorkspaceCard
+            <UnavailableResource />
+            {/* <WorkspaceCard
                 cardTitle={'Summarize YouTube video'}
                 showIssueNotice={showSummarize && showNoHistoryVideoIssue}
                 closeIssueNotice={setShowNoHistoryVideoIssue}
@@ -473,7 +473,7 @@ const YouTubeSummarize = ({ showNoHistoryVideoIssue, setShowNoHistoryVideoIssue 
                         <ResultContentYTSummarize progressValue={progressValue} isLoading={isLoading} themeColor={themeColor} showSummarize={showSummarize} currentSummarize={history ? history[historyId] : []} />
                     </>
                 }
-            </WorkspaceCard>
+            </WorkspaceCard> */}
         </>
     )
 }
